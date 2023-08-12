e15c9b052ea8556636d33aa1224407c4c7366bf6
jake <92687743+jayakanth-jake@users.noreply.github.com>
2023-08-12 08:37:14 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,15 +1,44 @@
-// create a web server
-// 1. import express
-const express = require('express')
-// 2. create a server
-const app = express()
-// 3. create a port
-const port = 3000
-// 4. create a route
-app.get('/', (req, res) => {
-  res.send('列出全部 Todo')
+// create a web server using node.js
+// run using node comments.js
+// using express.js
+// https://expressjs.com/
+// https://www.npmjs.com/package/express
+// https://www.npmjs.com/package/body-parser
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/index.html', function (req, res) {
+   res.sendFile( __dirname + "/" + "index.html" );
 })
-// 5. listen to the port
-app.listen(port, () => {
-  console.log(`App running on port ${port}`)
+
+app.get('/getComments', function (req, res) {
+   var comments = [
+      { "name":"John", "comment":"I like this article"},
+      { "name":"Mary", "comment":"I like this article too"}
+   ];
+   res.end( JSON.stringify(comments));
+})
+
+app.post('/postComment', urlencodedParser, function (req, res) {
+   // Prepare output in JSON format
+   var comment = {
+      name:req.body.name,
+      comment:req.body.comment
+   };
+   console.log(comment);
+   res.end( JSON.stringify(comment));
+})
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Web server listening at http://%s:%s", host, port)
 })
